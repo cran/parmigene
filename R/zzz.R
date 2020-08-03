44fb5a917237e0ef8ce95cdf4f0c997911b222c0
Gabriele Sales <gabriele.sales@unipd.it>
2020-08-03 08:50:03 +0000

    version 1.1.0

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-# Copyright 2010-2012 Gabriele Sales <gabriele.sales@unipd.it>
+# Copyright 2010-2020 Gabriele Sales <gabriele.sales@unipd.it>
 #
 #
 # This file is part of parmigene.
@@ -15,34 +15,33 @@
 # You should have received a copy of the GNU Affero General Public
 # License along with parmigene. If not, see <http://www.gnu.org/licenses/>.
 
-
-.mi_single <- function(x, y, k, noise)
-  .C("mi_single",
+mi_single <- function(x, y, k, noise) {
+  .C(C_mi_single,
      as.double(x),
      as.double(y),
      as.integer(length(x)),
      as.integer(k),
      as.double(noise),
      res = double(1),
-     PACKAGE = "parmigene",
-     DUP = TRUE)$res
+     PACKAGE = "parmigene")$res
+}
 
-.mi_cross <- function(xs, ys, k, noise) {
+mi_cross <- function(xs, ys, k, noise) {
   h1 <- nrow(xs)
   h2 <- nrow(ys)
   w  <- ncol(xs)
-  
-  res <- .C("mi_cross",
-            as.double(t(xs)),
-            as.integer(h1),
-            as.double(t(ys)),
-            as.integer(h2),
-            as.integer(w),
-            as.integer(k),
-            as.double(noise),
-            res = double(h1*h2),
-            PACKAGE = "parmigene",
-            DUP = TRUE)$res
+
+  res <-
+    .C(C_mi_cross,
+       as.double(t(xs)),
+       as.integer(h1),
+       as.double(t(ys)),
+       as.integer(h2),
+       as.integer(w),
+       as.integer(k),
+       as.double(noise),
+       res = double(h1*h2),
+       PACKAGE = "parmigene")$res
 
   m <- t(matrix(res, nrow=h2))
   rownames(m) <- rownames(xs)
@@ -50,19 +49,19 @@
   m
 }
 
-.mi_all <- function(xs, k, noise) {
+mi_all <- function(xs, k, noise) {
   h <- nrow(xs)
   w <- ncol(xs)
-  
-  res <- .C("mi_all",
-            as.double(t(xs)),
-            as.integer(h),
-            as.integer(w),
-            as.integer(k),
-            as.double(noise),
-            res = double(h*h),
-            PACKAGE = "parmigene",
-            DUP = TRUE)$res
+
+  res <-
+    .C(C_mi_all,
+       as.double(t(xs)),
+       as.integer(h),
+       as.integer(w),
+       as.integer(k),
+       as.double(noise),
+       res = double(h*h),
+       PACKAGE = "parmigene")$res
 
   m <- matrix(res, nrow=h)
   colnames(m) <- rownames(xs)
@@ -70,15 +69,15 @@
   m
 }
 
-.aracne <- function(mis, n, eps, eta) {
-  res <- .C("aracne",
-            as.double(t(mis)),
-            as.integer(n),
-            as.double(eps),
-            as.double(eta),
-            res=double(n*n),
-            PACKAGE = "parmigene",
-            DUP = FALSE)$res
+aracne_impl<- function(mis, n, eps, eta) {
+  res <-
+    .C(C_aracne,
+       as.double(t(mis)),
+       as.integer(n),
+       as.double(eps),
+       as.double(eta),
+       res=double(n*n),
+       PACKAGE = "parmigene")$res
 
   m <- matrix(res, nrow=n)
   colnames(m) <- rownames(mis)
@@ -86,13 +85,13 @@
   m
 }
 
-.clr <- function(mis, n) {
-  res <- .C("clr",
-            as.double(t(mis)),
-            as.integer(n),
-            res = double(n*n),
-            PACKAGE = "parmigene",
-            DUP = FALSE)$res
+clr_impl <- function(mis, n) {
+  res <-
+    .C(C_clr,
+       as.double(t(mis)),
+       as.integer(n),
+       res = double(n*n),
+       PACKAGE = "parmigene")$res
 
   m <- matrix(res, nrow=n)
   colnames(m) <- rownames(mis)
@@ -100,13 +99,13 @@
   m
 }
 
-.mrnet <- function(mis, n) {
-    res <- .C("mrnet",
-              as.double(t(mis)),
-              as.integer(n),
-              res = double(n*n),
-              PACKAGE = "parmigene",
-              DUP = FALSE)$res
+mrnet_impl <- function(mis, n) {
+    res <-
+      .C(C_mrnet,
+         as.double(t(mis)),
+         as.integer(n),
+         res = double(n*n),
+         PACKAGE = "parmigene")$res
 
   m <- matrix(res, nrow=n)
   colnames(m) <- rownames(mis)
